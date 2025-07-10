697f2d7c455afb524e3d385745b8cb26849cca8b
rhuder <munozallan19@gmail.com>
2025-07-10 01:45:08 -0700

    Initial commit

---
+++ src/main/java/com/rhuder/coolmod/CoolModClient.java
@@ -0,0 +1,10 @@
+package com.rhuder.coolmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoolModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
